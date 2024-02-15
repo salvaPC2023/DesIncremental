550885f94b2345665ced25468fd76eb593e628ed
AleBustamante <adbdc.u@gmail.com>
2024-02-15 09:58:43 -0400

    Anadidad saludo por edad

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,7 @@
 import saludar from "./saludador";
 
 const nombre_input = document.querySelector("#nombre");
+const edad_input = document.querySelector("#edad")
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -8,6 +9,12 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombre = nombre_input.value;
+  const edad = edad_input.value;
 
-  div.innerHTML = "<p>" + "Hola, " + saludar(nombre) + "</p>";
+  if (edad > 30) {
+    div.innerHTML = "<p>" + "Hola, señor " + saludar(nombre) + "</p>";
+  }
+  else {
+    div.innerHTML = "<p>" + "Hola, " + saludar(nombre) + "</p>";
+  }
 });
