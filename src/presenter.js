8fcd6863d7842a37c6cabed63f480a4874f213d9
AleBustamante <adbdc.u@gmail.com>
2024-02-15 10:26:59 -0400

    Anadida funcionalidad saludo por genero

--- src/presenter.js
+++ src/presenter.js
@@ -1,7 +1,9 @@
 import saludar from "./saludador";
 
 const nombre_input = document.querySelector("#nombre");
-const edad_input = document.querySelector("#edad")
+const edad_input = document.querySelector("#edad");
+const generom_input = document.querySelector("#generom");
+const generof_input = document.querySelector("#generof");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -10,9 +12,17 @@ form.addEventListener("submit", (event) => {
 
   const nombre = nombre_input.value;
   const edad = edad_input.value;
+  var generom = generom_input;
+  var generof = generof_input;
 
   if (edad > 30) {
-    div.innerHTML = "<p>" + "Hola, señor " + saludar(nombre) + "</p>";
+    if(generom.checked){
+      div.innerHTML = "<p>" + "Hola, señor " + saludar(nombre) + "</p>";
+    }
+    else{
+      div.innerHTML = "<p>" + "Hola, señora " + saludar(nombre) + "</p>";
+    }
+
   }
   else {
     div.innerHTML = "<p>" + "Hola, " + saludar(nombre) + "</p>";
